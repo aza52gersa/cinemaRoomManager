4084ea1ba950d57e4352214b170bc4c3555025b8
Aza52Gersa <aza52gersa@gmail.com>
2023-05-23 11:11:06 +0500

    Changing in the whole project. Replacing just one line of code with functions in order to make the code look simpler.

--- src/Cinema.java
+++ src/Cinema.java
@@ -1,74 +1,86 @@
 import java.util.Scanner;
 
 public class Cinema {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of rows:");
         int rows = scanner.nextInt();
         System.out.println("Enter the number of seats in each row:");
-        int numberOfSeats = scanner.nextInt();
-        String[][] cinema = new String[rows][numberOfSeats];
+        int seats = scanner.nextInt();
+        String[][] cinema = new String[rows][seats];
+        for (int i = 0; i < rows; i++)
+            for(int k = 0; k < seats; k++)
+                cinema[i][k] = "S";
 
-        int number = 1;
+        boolean exitBoolean = false;
+        while(!exitBoolean) {
+            System.out.println();
+            System.out.println(
+                    """
+                    1. Show the seats
+                    2. Buy a ticket
+                    0. Exit
+                    """);
+            int userInput = scanner.nextInt();
+            switch (userInput) {
+                case 1:
+                    showSeats(cinema);
+                    break;
+
+                case 2:
+                    System.out.println("Enter a row number: ");
+                    int rowNumber = scanner.nextInt();
+                    System.out.println("Enter a seat number in that row: ");
+                    int seatNumber = scanner.nextInt();
+                    buyTicket(cinema, rowNumber, seatNumber);
+                    break;
+
+                case 0:
+                    exitBoolean = true;
+                    break;
+
+                default:
+                    System.out.println("You entered the wrong number!");
+                    exitBoolean = true;
+            }
+        }
+    }
+
+    public static void showSeats(String[][] cinema) {
+        String[][] cinemaDuplicate = cinema;
+        int horizontalNumbers = 1;
+        System.out.println();
         System.out.println("Cinema:");
         System.out.print("  ");
-        while (number <= numberOfSeats){
-            System.out.print(number + " ");
-            number++;
+        while (horizontalNumbers <= cinemaDuplicate[0].length) {
+            System.out.print(horizontalNumbers + " ");
+            horizontalNumbers++;
         }
 
-        int number2 = 1;
         System.out.println();
-        for (int i = 0; i < rows; i++) {
-            System.out.print(number2 + " ");
-            for (int j = 0; j < numberOfSeats; j++) {
-                cinema[i][j] = "S";
-                System.out.print(cinema[i][j] + " ");
+        int verticalNumbers = 1;
+        for (int i = 0; i < cinemaDuplicate.length; i++) {
+            System.out.print(verticalNumbers + " ");
+            for (int j = 0; j < cinemaDuplicate[i].length; j++) {
+                System.out.print(cinemaDuplicate[i][j] + " ");
             }
             System.out.println();
-            number2++;
+            verticalNumbers++;
         }
-
-        System.out.println();
-        System.out.println("Enter a row number:");
-        int userRow = scanner.nextInt();
-        System.out.println("Enter a seat number in that row:");
-        int userSeat = scanner.nextInt();
-        System.out.println();
-
-        if (rows * numberOfSeats < 60)
+    }
+    public static void buyTicket(String[][] cinema, int rowNumber, int seatNumber) {
+        cinema[rowNumber - 1][seatNumber - 1] = "B";
+        ticketPrice(cinema, rowNumber);
+    }
+    public static void ticketPrice(String[][] cinema, int rowNumber) {
+        int totalSeats = cinema.length * cinema[0].length;
+        if (totalSeats < 60) {
             System.out.println("Ticket price: $10");
-        else
-        if (rows / 2 >= userRow)
-            System.out.println("Ticket price: $10");
-        else
-            System.out.println("Ticket price: $8");
-
-        System.out.println();
-        System.out.println("Cinema:");
-        System.out.print("  ");
-        int number3 = 1;
-        while (number3 <= numberOfSeats){
-            System.out.print(number3 + " ");
-            number3++;
-        }
-
-        int number4 = 1;
-        System.out.println();
-        for (int i = 0; i < rows; i++) {
-            System.out.print(number4 + " ");
-            for (int j = 0; j < numberOfSeats; j++) {
-                if (i == userRow - 1 && j == userSeat - 1) {
-                    cinema[i][j] = "B";
-                    System.out.print(cinema[i][j] + " ");
-                } else {
-                    cinema[i][j] = "S";
-                    System.out.print(cinema[i][j] + " ");
-                }
-            }
-            System.out.println();
-            number4++;
+        } else {
+            if (cinema.length / 2 >= rowNumber)
+                System.out.println("Ticket price: $10");
+            else
+                System.out.println("Ticket price: $8");
         }
     }
 }
